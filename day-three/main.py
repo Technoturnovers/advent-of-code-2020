0380176c7235516001a1c7caed2ae5246f2898bc
Technoturnovers <technoturnovers@gmail.com>
2020-12-03 16:24:10 -0500

    Completed Day 3, Part Two

--- day-three/main.py
+++ day-three/main.py
@@ -6,13 +6,12 @@ def input(path: str) -> list:
             return [ line.rstrip() for line in file ]
     except Exception as e:
         print(repr(e))
+        sys.exit()
 
-def partone(map: list) -> int:
+def trees(map: list, x_slope: int, y_slope: int) -> int:
     count = 0
     x = 0
-    x_slope = 3
     y = 0
-    y_slope = 1
     while y < len(map) - 1:
         x += x_slope
         y += y_slope
@@ -20,6 +19,14 @@ def partone(map: list) -> int:
             count += 1
     return count
 
+def part_two(map: list) -> int:
+    product = 1
+    slopes = [ (1, 1), (3, 1), (5, 1), (7, 1), (1, 2) ]
+    for pair in slopes:
+        product *= trees(map, pair[0], pair[1])
+    return product
+
 if __name__ == "__main__":
-    map = input('input.txt')
-    print(partone(map))
+    map = input('day-three/input.txt')
+    print(trees(map, 3, 1)) # Part One
+    print(part_two(map)) # Part Two
