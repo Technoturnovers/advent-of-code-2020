9ad022fa152d0685ab7000b8444771255e3aa889
Technoturnovers <technoturnovers@gmail.com>
2020-12-04 16:28:57 -0500

    why isn't part 2 working reeeeeeeeeee

--- day-four/main.py
+++ day-four/main.py
@@ -1,3 +1,4 @@
+import re
 import sys
 
 def read_file(path: str) -> str:
@@ -8,15 +9,48 @@ def read_file(path: str) -> str:
         print(repr(e))
         sys.exit()
 
-def part_one(blob: str) -> int:
+def part_one(blob: str) -> list:
     entries = blob.split('\n\n')
-    counter = 0
+    valid = []
     required = ['byr:', 'iyr:', 'eyr:', 'hgt:', 'hcl:', 'ecl:', 'pid:']
     for entry in entries:
         if all(substr in entry for substr in required):
-            counter += 1
-    return counter
+            valid.append(entry.strip())
+    return valid
+
+def part_two(entries: list) -> list:
+    counter = 0
+    entries = [{field.split(':')[0]:field.split(':')[1] for field
+               in entry.split()} for entry in entries]
+    valid = []
+    for entry in entries:
+        if not 1920 <= int(entry['byr']) <= 2002:
+            continue
+        if not 2010 <= int(entry['iyr']) <= 2020:
+            continue
+        if not 2020 <= int(entry['eyr']) <= 2030:
+            continue
+        if entry['hgt'].endswith('cm'):
+            if not 150 <= int(entry['hgt'][:-2]) <= 193:
+                continue
+        if entry['hgt'].endswith('in'):
+            if not 59 <= int(entry['hgt'][:-2]) <= 76:
+                continue
+        if not re.match(r'#[a-f0-9]{6}', entry['hcl']):
+            continue
+        hair_colors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+        if not any(substr in entry['ecl'] for substr in hair_colors):
+            continue
+        if not re.match(r'\d{9}', entry['pid']):
+            continue
+        print(entry)
+        valid.append(entry)
+    return valid
+
 
 if __name__ == '__main__':
     blob = read_file('day-four/input.txt')
-    print(part_one(blob))
+    valid = part_one(blob)
+    print(len(valid))
+    valid = part_two(valid)
+    print(len(valid))
