930bb117ec241f3223d0fc3815206db5b7daeca0
Technoturnovers <technoturnovers@gmail.com>
2020-12-04 12:32:01 -0500

    Solved Day 4, Part One

---
+++ day-four/main.py
@@ -0,0 +1,23 @@
+import re
+import sys
+
+def read_file(path: str) -> str:
+    try:
+        with open(path) as file:
+            return file.read()
+    except Exception as e:
+        print(repr(e))
+        sys.exit()
+
+def part_one(blob: str) -> int:
+    entries = blob.split('\n\n')
+    counter = 0
+    required = ['byr:', 'iyr:', 'eyr:', 'hgt:', 'hcl:', 'ecl:', 'pid:']
+    for entry in entries:
+        if all(substr in entry for substr in required):
+            counter += 1
+    return counter
+
+if __name__ == '__main__':
+    blob = read_file('day-four/input.txt')
+    print(part_one(blob))
